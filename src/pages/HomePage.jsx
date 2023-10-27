9b8e27b1f951afb3992248c20c01379756610b02
alextor26 <alextor2625@gmail.com>
2023-10-27 13:44:22 -0400

    Finished styling redeploying 15

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,21 @@
-import { Container, Row, Col, Jumbotron, Button } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const HomePage = () => {
   return (
-    <Container>
+    <Container className="text-center" style={{ minHeight: "100vh" }}>
       <Row className="justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
         <Col>
-          <Jumbotron>
-            <h1 className="display-3 text-center">Welcome to CookBook</h1>
-            <p className="lead text-center">Discover and share delicious recipes.</p>
-            <img
-              src="https://res.cloudinary.com/dg2rwod7i/image/upload/v1698428024/Cookbook/cookbook-gallery/ay8t36ndyslphr5lmljk.jpg"
-              alt="HomePageLogo"
-              className="img-fluid"
-            />
-            <p className="text-center">
-              <Button variant="primary">Get Started</Button>
-            </p>
-          </Jumbotron>
+          <h1 className="display-3">Welcome to CookBook</h1>
+          <p className="lead">Discover and share delicious recipes.</p>
+          <img
+            src="https://res.cloudinary.com/dg2rwod7i/image/upload/v1698428024/Cookbook/cookbook-gallery/ay8t36ndyslphr5lmljk.jpg"
+            alt="HomePageLogo"
+            className="img-fluid"
+          />
+          <p>
+            <Button variant="primary">Get Started</Button>
+          </p>
         </Col>
       </Row>
     </Container>
