0c9659738a526895f2af21cac5c4a4840eee410f
alextor26 <alextor2625@gmail.com>
2023-10-27 13:38:57 -0400

    Finished styling redeploying 14

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
+import { Container, Row, Col, Jumbotron, Button } from "react-bootstrap";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container>
+      <Row className="justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+        <Col>
+          <Jumbotron>
+            <h1 className="display-3 text-center">Welcome to CookBook</h1>
+            <p className="lead text-center">Discover and share delicious recipes.</p>
+            <img
+              src="https://res.cloudinary.com/dg2rwod7i/image/upload/v1698428024/Cookbook/cookbook-gallery/ay8t36ndyslphr5lmljk.jpg"
+              alt="HomePageLogo"
+              className="img-fluid"
+            />
+            <p className="text-center">
+              <Button variant="primary">Get Started</Button>
+            </p>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
